49a768b8c139d3944693b234fde385ba124141d0
nerru <75166617+nerru@users.noreply.github.com>
2023-10-27 13:36:28 +0900

    Add files via upload

--- python_Simple-main/project_parking_tower/main.py
+++ python_Simple-main/project_parking_tower/main.py
@@ -45,13 +45,29 @@ while True:
                tower[i] = car_num
                car_cnt += 1
                break
-        else:
-           print("MSG: 타원가 만차입니다. 다음에 이용해주세요.")
+      else:
+           print("MSG: 타워가 만차입니다. 다음에 이용해주세요.")
     # 입고할 차량 번호 입력
     # 입고
     elif select_num ==2:
-      pass
-    elif select_num ==3:
-      pass
+      # 1. 출고 차량 번호 입력
+       car_num = input(">>출고: ")
+       if car_num in tower:
+          for i, car in enumerate(tower):
+              if car == car_num:
+                  tower[i] = "" # 차량 제거
+                  car_cnt -= 1
+                  break
+      else:
+          print("MSG: 해당 번호로 입고된 차량이 없습니다.")
+      # 2. tower == 출고 차량 번호 match
+      # 3. yes: 출고(tower  제거(""), car_cnt-1)
+      # 3-2. no: MSG(입고 된 차량x)
+
+     elif select_num ==3:    #조회
+        print("==주차 타워 현황 ==")
+        for i in range(len(tower)-1, -1, -11):
+            print(f" > {i+1}층 {tower[i]}")
     elif select_num == 4:
      print("MSG: 프로그램을 종료합니다.")
+     exit()
