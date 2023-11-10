88999e4558a52bf4e88e0cab22c33a9aa94434a0
nerru <75166617+nerru@users.noreply.github.com>
2023-11-10 14:40:54 +0900

    Add files via upload

--- python_Simple-main/project_daum_news/db/common/connection.py
+++ python_Simple-main/project_daum_news/db/common/connection.py
@@ -22,8 +22,46 @@
 #    - ID and PW
 #   2. SQL을 사용해서 작업(CRUD) 진행
 #    -SQL(구조질의어): RDB를 사용하기 위해서는 반드시 사용!
+#    -RDB(SQL)을 사용, NoSQL(SQL x)
 #    CREATE    -> INSERT
 #    READ      -> SELECT
 #    UPDATE    -> UPDATE
 #    DELETE    -> DELETE
 
+# ** MongoDB  사용 방법 2가지
+#  1. 직접 설치(Local)
+#  - IP, PORT, ID, PW
+#  - 단점: 설치 과정 복잡, 설정 직접, 컴퓨터 자원 부족
+#  - 장점: 사용 편함, 관리 편함, 커스터 마이징 가능
+#  2. MongoDB에서 제공하는 Web Cloud 사용
+#  - URL, ID, PW
+#  - 장점: 사용 편함, 설치 x, 설정 x, 컴퓨터 자원 x
+#  - 단점: 관리 x, 커스터마이징 x
+
+# **d MongoDB 구조
+# 설치: MongoDB(DBMS)  : 시스템
+#     ㄴ DB(카카오톡) : 폴더
+#           ㄴ Collection(회원)
+#           ㄴ Collection(톡)
+#           ㄴ Collection(친구)
+#           ㄴ ...
+#     ㄴ DB(카카오뱅크) : 폴더
+#           ㄴ Collection(회원)
+#           ㄴ Collection(계좌)
+#           ㄴ Collection(대출)
+#           ㄴ ...
+#     ㄴ DB(카카오페이) : 폴더
+#           ㄴ...
+
+# pymongo: Python - MongoDB 연결해서 사용
+from pymongo import MongoClient
+
+# MongoDB connection
+def conn_mongodb():
+    # URL, ID, PW
+    DB_ID = "root"  # 상수(전체 대문자로 변수명을 사용)
+    DB_PW = "1234"  # 예시) 은행에서 금리(상수)
+    Client = MongoClient(f"mongodb+srv://{DB_ID}:{DB_PW}@daumcluster.ebgcef1.mongodb.net/")  # URL
+    db = Client["daum"]
+    collection = db.get_collection("news")
+    return collection
--- python_Simple-main/project_daum_news/service/service_news.py
+++ python_Simple-main/project_daum_news/service/service_news.py
@@ -1,7 +1,7 @@
 # 해당 URL 접속해서 기사 [제목, 본문, 날짜] 수집하는 함수
 import requests
 from bs4 import BeautifulSoup
-
+from db.news_dao import add_news
 
 def get_news(url: str):
     result = requests.get(url)
@@ -24,3 +24,12 @@ def get_news(url: str):
 
         content += p.get_text()
     print(f"본문: {content}")
+
+    # 수집한 데이터 DB에 저장
+    # MongoDB -> JSON = Dict Type
+    data = {
+        "title": title ,
+        "content": content ,
+        "date": reg_date
+    }
+    add_news(data)
